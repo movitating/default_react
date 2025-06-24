e595d798a6b471957e8a293ef13d9b18e62600e9
youngjune <jjmullan24@gmail.com>
2025-06-24 17:16:11 +0900

    🐳 chore: tanstack 라이브러리 설치 및 main.tsx 초기 설정

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@app/styles/style.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from '@app/App';
+import '@app/styles/style.css';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>
 );
